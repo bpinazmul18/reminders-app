742182fa66d4e82c2de05df79ff647197822d384
Nazmul Haque <bpinazmul18@gmail.com>
2022-08-09 01:14:50 +0600

    Playing bootstrap buttons.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container py-5'>
+      <Button variant='primary' size='sm'>Click me!</Button>
     </div>
   );
 }
