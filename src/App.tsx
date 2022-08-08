4585bc5da4e9f5cb46fbb57c416a25a560c2c4de
Nazmul Haque <bpinazmul18@gmail.com>
2022-08-09 01:41:04 +0600

    Using the state hook.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReminderList from './components/ReminderList';
 import Reminder from './models/reminder';
 
-const reminders: Reminder[] = [
-  {id: 1, title: 'Reminder 1'}
-]
-
 function App() {
+  const [reminders, setReminders] = useState<Reminder[]>([
+    {id: 1, title: 'Reminder 1'}
+  ])
+
   return (
     <div className='container py-5'>
       <ReminderList items={reminders}/>
