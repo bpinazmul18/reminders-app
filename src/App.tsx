613a8907bd500ccbf194038432ec9c66e729eb45
Nazmul haque <bpinazmul18@gmail.com>
2022-08-09 12:26:33 +0600

    Handling newReminder.

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,56 @@
 import React, { useEffect, useState } from 'react';
 import ReminderList from './components/ReminderList';
 import Reminder from './models/reminder';
-import { fetchReminders, deleteReminder } from './services/reminders';
+import {fetchReminders, deleteReminder, addReminder} from './services/reminders';
 import NewReminder from "./components/NewReminder";
 
 function App() {
-  const [reminders, setReminders] = useState<Reminder[]>([])
+      const [reminders, setReminders] = useState<Reminder[]>([])
 
-  useEffect(() => {
-    loadReminders()
-  }, [])
+      useEffect(() => {
+        loadReminders()
+      }, [])
 
-  const loadReminders = async () => {
-    const response = await fetchReminders()
-    setReminders(response.data)
-  }
+      const loadReminders = async () => {
+        const response = await fetchReminders()
+        setReminders(response.data)
+      }
 
-  const handleReminder = async (id: number) => {
-    try {
-       const newReminders = reminders.filter((reminder) => reminder.id !== id)
-       setReminders(newReminders)
+      const handleReminder = async (id: number) => {
+        try {
+           const newReminders = reminders.filter((reminder) => reminder.id !== id)
+           setReminders(newReminders)
 
-       await deleteReminder(id)
-   } catch (ex) {
-      //  if (ex.response && ex.response.status === 404)
-      //      console.log('This post already been deleted!')
+           await deleteReminder(id)
+       } catch (ex) {
+          //  if (ex.response && ex.response.status === 404)
+          //      console.log('This post already been deleted!')
 
-      //   if (ex.response && ex.response.status === 403)
-      //       console.log(ex.response.data)
+          //   if (ex.response && ex.response.status === 403)
+          //       console.log(ex.response.data)
 
-       setReminders(reminders)
-   }
-}
+           setReminders(reminders)
+       }
+    }
+
+    const handleNewReminder = async (title: string) => {
+        try {
+            const response = await addReminder(title)
+            setReminders([response.data, ...reminders])
+        } catch (ex) {
+            //  if (ex.response && ex.response.status === 404)
+            //      console.log('This post already been deleted!')
+
+            //   if (ex.response && ex.response.status === 403)
+            //       console.log(ex.response.data)
+
+            setReminders(reminders)
+        }
+    }
 
   return (
     <div className='container py-5'>
-        <NewReminder/>
+        <NewReminder onAddReminder={handleNewReminder}/>
       <ReminderList items={reminders} onRemoveReminder={handleReminder}/>
     </div>
   );
--- src/components/NewReminder.tsx
+++ src/components/NewReminder.tsx
@@ -1,10 +1,21 @@
 import React, {useState} from "react";
 import {Button, Form} from "react-bootstrap";
 
-function NewReminder (): JSX.Element {
+interface NewReminderProps {
+    onAddReminder: (title: string) => void
+}
+
+function NewReminder ({ onAddReminder }: NewReminderProps): JSX.Element {
     const [title, setTitle] = useState('')
+
+    const submitForm = (e: React.FormEvent) => {
+        e.preventDefault()
+        if (!title)return;
+        onAddReminder(title)
+        setTitle('')
+    }
     return (
-        <Form className="mb-4">
+        <Form onSubmit={submitForm} className="mb-4">
             <Form.Label htmlFor="title">Email address</Form.Label>
             <Form.Control type="text" name="title" value={title} onChange={(e)=> setTitle(e.target.value)} id="title" className="mb-3" placeholder="Enter title..." />
             <Button variant="primary" type="submit" className="rounded-pill">
